36c96e48d85f25d83b6585fc35cdf0fdb37d148f
Jose Montero <josemariodev@MacBook-Pro.attlocal.net>
2019-08-07 11:41:17 -0500

    tested endpoints, added query parameters

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,22 +1,7 @@
-// inside /users/userRoutes.js <- this can be place anywhere and called anything
 const express = require('express');
 
-const router = express.Router(); // notice the Uppercase R
+const router = express.Router();
 const Posts = require('../data/db');
-
-// _______________________________________________________________________________
-
-router.get('/', (req, res) => {
-  Posts.find()
-    .then(posts => {
-      res.status(200).json(posts);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: 'The posts information could not be retrieved.' });
-    });
-});
 
 // _______________________________________________________________________________
 
@@ -75,24 +60,15 @@ router.post('/:id/comments', (req, res) => {
 
 // _______________________________________________________________________________
 
-router.get('/:id', (req, res) => {
-  const postId = req.params.id;
-  posts
-    .findById(postId)
-    .first()
-    .then(post => {
-      if (!post) {
-        return res
-          .status(404)
-          .json({ message: 'The post with the specified ID does not exist.' });
-      } else {
-        res.status(200).json(post);
-      }
+router.get('/', (req, res) => {
+  Posts.find()
+    .then(posts => {
+      res.status(200).json(posts);
     })
     .catch(err => {
       res
         .status(500)
-        .json({ error: 'The post information could not be retrieved.' });
+        .json({ error: 'The posts information could not be retrieved.' });
     });
 });
 
@@ -100,7 +76,6 @@ router.get('/:id', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const postId = req.params.id;
-  console.log(req.params.id);
   posts
     .findById(postId)
     .first()
--- server.js
+++ server.js
@@ -6,4 +6,11 @@ const postsRouter = require('./routes/postsRouter');
 server.use(express.json());
 server.use('/api/posts', postsRouter);
 
+server.get('/', (req, res) => {
+  const queryParameters = req.query;
+  res
+    .status(200)
+    .json({ message: 'query parameters working', queryParameters });
+});
+
 server.listen(8000, () => console.log('port running on 8000'));
