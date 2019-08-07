c8b54754ac42d59fb19ebdf1bbcc89ab4bb394b6
Jose Montero <josemariodev@MacBook-Pro.attlocal.net>
2019-08-07 11:05:19 -0500

    GET, POST, and POST comments = working

---
+++ routes/postsRouter.js
@@ -0,0 +1,77 @@
+// inside /users/userRoutes.js <- this can be place anywhere and called anything
+const express = require('express');
+
+const router = express.Router(); // notice the Uppercase R
+const Posts = require('../data/db');
+
+// _______________________________________________________________________________
+
+router.get('/', (req, res) => {
+  Posts.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' });
+    });
+});
+
+// _______________________________________________________________________________
+
+router.post('/', (req, res) => {
+  const newPost = req.body;
+  if (!newPost.title || !newPost.contents) {
+    res.status(400).json({
+      errorMessage: 'Please provide title and contents for the post.',
+    });
+  }
+  Posts.insert(newPost)
+    .then(post => {
+      res.status(201).json(post);
+      console.log(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: 'There was an error while saving the post to the database',
+      });
+    });
+});
+
+// _______________________________________________________________________________
+
+router.post('/:id/comments', (req, res) => {
+  const postId = req.params.id;
+  const { text } = req.body;
+  posts
+    .findById(postId)
+    .first()
+    .then(post => {
+      if (!post) {
+        return res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        if (!text) {
+          res
+            .status(400)
+            .json({ errorMessage: 'Please provide text for the comment.' });
+        } else {
+          posts.insertComment(req.body);
+          return then(comment => {
+            res.status(201).json(comment);
+          });
+        }
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
+// _______________________________________________________________________________
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const Posts = require('./data/db.js');
 const server = express();
+
+const postsRouter = require('./routes/postsRouter');
+
 server.use(express.json());
+server.use('/api/posts', postsRouter);
 
 server.listen(8000, () => console.log('port running on 8000'));
